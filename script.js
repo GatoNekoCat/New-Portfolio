70e7419eca05b0e4bca21a4db37e0c838cd372c5
Raiicon <orankai@gmail.com>
2020-12-06 03:09:49 -0500

    real good stuff

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ open.addEventListener('click',() => container.classList.add('show-nav'))
 
 
 close.addEventListener('click',() => container.classList.remove('show-nav'))
+
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel)=> {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
